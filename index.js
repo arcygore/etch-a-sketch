1be4912540f660e7c54a2d2ad2bbf7ccf818cd71
Ryan <rcgore602@gmail.com>
2022-05-06 14:26:19 -0400

    Added additional functionality, but could not get it to work
    Was not in scope or intent of project guidelines
    
    Commented out. Can work, but is not fully functional

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let mousePos = {x:0, y:0};
+
 function createBoard() {
     boardArea = document.querySelector('.board');
     for (let i = 1; i <=16; i++) {
@@ -8,13 +10,35 @@ function createBoard() {
     return block = document.querySelectorAll('.board-block');
 }
 
+function draw(block) {}
+
 function colorChange(block) {
     for (let i = 0; i < block.length; i++) {
         block[i].addEventListener('mouseover', function() {
             block[i].style.backgroundColor = 'blue';
         })
-    }
+    }   
 }
+
+
+// function colorChange2(block) {
+    // for (let i = 0; i < block.length; i++) {
+    //     block[i].addEventListener('mousemove', function(e) {
+    //         let xPos = e.offsetX;
+    //         let yPos = e.offsetY;
+    //         console.log(`${xPos}, ` + ` ${yPos}`)
+    //         drawn = document.createElement('div');
+    //         drawn.className = '.drawn';
+    //         drawn.style.position = 'absolute';
+    //         drawn.style.height = '5px';
+    //         drawn.style.width = '5px';
+    //         drawn.style.top = yPos+'px';
+    //         drawn.style.left = xPos+'px';
+    //         drawn.style.backgroundColor = 'white';
+    //         block[i].appendChild(drawn);
+//         })
+//     }
+// }
 
 
 
