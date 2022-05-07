c6f4275d036abd39b8ce29f90d4b94f6241c3753
Ryan <rcgore602@gmail.com>
2022-05-07 12:09:33 -0400

    Completed project commit Comments updated to indicate functionality/attempts Missing optional rainbow functionality -- Will return

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 Beginning this project, I am coming off the UI development for the Rock, Paper, Scissors project. That was a really fun modification to a previous project, and I feel that I learned a lot about what you can do with the different query selector methods. JavaScript has been far more interesting to me than HTML/CSS, as my previous experience in C, Java, Python have given me a more programming minded brain over the markup languages.
 
 I intend to learn a lot more about the different CSS pseudo selectors, as well as in JS, and to make mouse inputs into the screen more "powerful".
+
+05/07/2022 - 12 PM
+
+Pushing this project with its "final" commit now. I use final with quotation marks because there is still some functionality I want to add, but I already know how to do it, and would rather move on to the next lessons since this project meets the intent of the assignment, and I want to come back to this project and add the functionality after I've learned even more, so that I can apply all the concepts and continue to learn more.
+
+This project taught me a lot about querySelector(), addEventListener, and passing variables from functions into other functions. I also tried a different method where I captured the mouse's relative (x,y) coordinates and applied a square at their new relative coordinates, but was getting a bug where I think it was capturing the coordinates relative to the already "drawn" divs, so the coordinates would be (0,1) for example, and it would leave a square at the 0,1 coordinates within that "block's" boundaries.
+
+All in all, I'm feeling way more comfortable with the HTML and CSS side of things, as every single thing I tried worked as I imagined it in my head. JavaScript continues to be by far my favorite part of all of it, but this reignited interest in CSS in my head, as things went way more smoothly than any other project before. I'm excited to see what else I learn that I can apply to this project or others moving forward!
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let mousePos = {x:0, y:0};
+// let mousePos = {x:0, y:0};
 
-function createBoard() {
+// let userInput = document.querySelector('.user-input'); -- This needed to be within addEventListener function.
+let submitBtn = document.querySelector('.submit');
+let newNum = submitBtn.addEventListener('click', function() {
+    let userInput = document.querySelector('.user-input');
+    bl = createBoard(userInput.value);
+    colorChange(bl);
+})
+
+function createBoard(num) {
     boardArea = document.querySelector('.board');
-    for (let i = 1; i <=16; i++) {
+    boardArea.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    boardArea.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    boardArea.style.backgroundColor = 'rgb(201, 200, 198)';
+    for (let i = 1; i <=num*num; i++) {
         block = document.createElement('div');
         block.className  = 'board-block';
+        block.style.backgroundColor = 'rgb(201, 200, 198)';
         boardArea.appendChild(block);
     }
     return block = document.querySelectorAll('.board-block');
@@ -14,12 +26,14 @@ function draw(block) {}
 
 function colorChange(block) {
     for (let i = 0; i < block.length; i++) {
-        block[i].addEventListener('mouseover', function() {
-            block[i].style.backgroundColor = 'blue';
+        block[i].style.backgroundColor = 'rgb(201, 200, 198)';
+        block[i].addEventListener('mousemove', function() {
+            block[i].style.backgroundColor = 'rgb(35, 35, 35)';
         })
     }   
 }
 
+// Different functionality below, with xPos and yPos capture of mouse, but it was funky.
 
 // function colorChange2(block) {
     // for (let i = 0; i < block.length; i++) {
@@ -41,6 +55,10 @@ function colorChange(block) {
 // }
 
 
+// let userQuantity = parseInt((Math.floor(Math.random()*50) * 2)); --- This was put in to make sure code worked before, just generated random even number between 1-100.
 
-let bl = createBoard();
-colorChange(bl);
+
+// Original declaration of functions below to run the program.
+
+// let bl = createBoard(newNum);
+// colorChange(bl);
